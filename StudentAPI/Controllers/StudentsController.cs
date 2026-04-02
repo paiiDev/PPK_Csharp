c39ac6a9a7d9e36436f1a431b7e2acd9c16211cc
Paii <pyaepaikyaw@gmail.com>
2026-04-02 20:48:30 +0630

    feat: Add Controller+Service+ADO.NET

---
+++ StudentAPI/Controllers/StudentsController.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using StudentAPI.Services;
+
+namespace StudentAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        private readonly StudentService _studentService;
+        public StudentsController(StudentService service){
+            _studentService = service;
+        }
+
+        [HttpGet]
+        public IActionResult GetStudents()
+        {
+            var students = _studentService.GetStudents();
+            return Ok(students);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetStudent(int id)
+        {
+            var student = _studentService.GetStudent(id);
+            if (student == null)
+            {
+                return NotFound("No data found");
+            }
+            return Ok(student);
+        }
+
+        [HttpPost]
+        public IActionResult Create(Student student)
+        {
+           _studentService.Create(student);
+            return Ok("Data created successfully.");
+
+        }
+
+        [HttpPatch("{id}")]
+        public IActionResult Update(int id, Student student)
+        {
+            var existingStudent = _studentService.GetStudent(id);
+            if (existingStudent == null)
+            {
+                return NotFound("No data found");
+            }
+            _studentService.Update(id, student);
+            return Ok("Data updated successfully.");
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var existingStudent = _studentService.GetStudent(id);
+            if (existingStudent == null)
+            {
+                return NotFound("No data found");
+            }
+            _studentService.Delete(id);
+            return Ok("Data deleted successfully.");
+        }
+        }
+}
---
+++ StudentAPI/Services/StudentService.cs
@@ -0,0 +1,113 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace StudentAPI.Services
+{
+    public class StudentService
+    {
+        private readonly string _connectionString;
+        public StudentService(IConfiguration configuration)
+        {
+            _connectionString = configuration.GetConnectionString("DbConnection");
+        }
+
+        public List<Student> GetStudents()
+        {
+            using (SqlConnection conn = new SqlConnection(_connectionString))
+            {
+
+                List<Student> students = new List<Student>();
+                conn.Open();
+
+                string Query = "SELECT Id, Name, Age FROM Students WHERE DeleteFlag = 0";
+                SqlCommand cmd = new SqlCommand(Query, conn);
+                SqlDataReader reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    students.Add(new Student
+                    {
+                        Id = Convert.ToInt32(reader["id"]),
+                        Name = reader["Name"].ToString(),
+                        Age = Convert.ToInt32(reader["Age"])
+                    });
+                }
+                return (students);
+            }
+        }
+
+        public Student GetStudent(int id)
+        {
+            using (SqlConnection conn = new SqlConnection(_connectionString))
+            {
+                Student student = null;
+
+                conn.Open();
+
+                string Query = "SELECT Id, Name, Age FROM Students WHERE Id = @Id AND DeleteFlag = 0";
+                SqlCommand cmd = new SqlCommand(Query, conn);
+
+                cmd.Parameters.AddWithValue("@Id", id);
+                SqlDataReader reader = cmd.ExecuteReader();
+
+                if (reader.Read())
+                {
+                    student = new Student
+                    {
+                        Id = Convert.ToInt32(reader["id"]),
+                        Name = reader["Name"].ToString(),
+                        Age = Convert.ToInt32(reader["Age"])
+                    };
+                }
+                return (student);
+            }
+        }
+
+        public void Create(Student student)
+        {
+            using (SqlConnection conn = new SqlConnection(_connectionString))
+            {
+                conn.Open();
+
+                string Query = "INSERT INTO Students (Name, Age) VALUES (@Name, @Age)";
+                SqlCommand cmd = new SqlCommand(Query, conn);
+
+                cmd.Parameters.AddWithValue("@Name", student.Name);
+                cmd.Parameters.AddWithValue("@Age", student.Age);
+
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        public void Update(int id, Student student)
+        {
+            using (SqlConnection conn = new SqlConnection(_connectionString))
+            {
+                conn.Open();
+
+                string Query = "UPDATE Students SET Name = @Name, Age = @Age WHERE Id = @Id AND DeleteFlag = 0";
+                SqlCommand cmd = new SqlCommand(Query, conn);
+
+                cmd.Parameters.AddWithValue("@Id", id);
+                cmd.Parameters.AddWithValue("@Name", student.Name);
+                cmd.Parameters.AddWithValue("@Age", student.Age);
+
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        public void Delete(int id)
+        {
+            using (SqlConnection conn = new SqlConnection(_connectionString))
+            {
+                conn.Open();
+
+                string Query = "UPDATE Students SET DeleteFlag = 1 WHERE Id = @Id";
+                SqlCommand cmd = new SqlCommand(Query, conn);
+
+                cmd.Parameters.AddWithValue("@Id", id);
+
+                cmd.ExecuteNonQuery();
+            }
+        }
+        }
+
+}
