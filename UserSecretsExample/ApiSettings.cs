f5c5a73781cfa9b4ef570744ee3a4ef955b482a8
Paii <pyaepaikyaw@gmail.com>
2026-04-12 11:58:37 +0630

    feat: add UserSecrets example

---
+++ UserSecretsExample/ApiSettings.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserSecretsExample
+{
+    public class ApiSettings
+    {
+        [Required]
+        [Url]
+        public string BaseUrl { get; set; }
+        [Required]
+        public string ApiKey { get; set; }
+    }
+}
---
+++ UserSecretsExample/Program.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
+using UserSecretsExample;
+
+var builder = Host.CreateDefaultBuilder(args)
+    .ConfigureAppConfiguration((context, config) =>
+    {
+        config.AddUserSecrets<Program>();
+    })
+    .ConfigureServices((context, services) =>
+    {
+        services.Configure<ApiSettings>(context.Configuration.GetSection("ApiSettings"));
+        services.AddOptions<ApiSettings>().Bind(context.Configuration.GetSection("ApiSettings"))
+        .ValidateDataAnnotations();
+    });
+
+var host = builder.Build();
+var mySettings = host.Services.GetRequiredService<IOptions<ApiSettings>>().Value;
+
+Console.WriteLine("--- Loading Settings ---");
+Console.WriteLine($"Base URL: {mySettings.BaseUrl}");
+Console.WriteLine($"API Key: {mySettings.ApiKey}");
+Console.ReadKey();
